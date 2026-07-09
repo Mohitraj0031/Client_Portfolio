1938d8e82d48b8d188a7e63f9ed6e3a5b9348b69
Mohitraj0031 <promosarthi91@gmail.com>
2026-07-09 18:19:43 +0530

    first subit to client

--- portfolio-main/src/components/Experience.jsx
+++ portfolio-main/src/components/Experience.jsx
@@ -12,17 +12,20 @@ const Experience = () => {
             <h2 className="text-center text-2xl font-semibold mb-2">
               Professional Experience
             </h2>
-            <ul className="mx-auto w-fit list-disc list-inside text-left space-y-1 text-lg">
-              <li>Developed and managed branch -wise productivity reports to support strategic decision-making related to branch expansion, consolidation,and operational performance analysis.</li>
-              <li>React.js</li>
-              <li>Responsive Design</li>
+            <ul className="mx-auto w-fit list-inside list-disc space-y-2 text-left text-lg">
+              <li>Developed and managed branch-wise productivity reports to support strategic decision-making on branch expansion, consolidation, and operational performance analysis.</li>
+              <li>Created and maintained investor data rooms containing critical business, financial, and portfolio-related information.</li>
+              <li>Prepared and updated investor presentations incorporating key business insights, financial performance metrics, operational metrics, and ratio analysis derived from financial statements.</li>
             </ul>
           </div>
           <div className="text-center border-2 border-gray-400 p-4 rounded-lg outline outline-transparent outline-2 hover:outline-white hover:outline-2 transition duration-300 hover:scale-105 hover:border-none hover:bg-gray-900 ">
-            <h2 className="text-2xl font-semibold mb-2">Backend Development</h2>
+            <h2 className="text-2xl font-semibold mb-2">Professinal Skills</h2>
             <ul className="mx-auto w-fit list-disc list-inside text-left space-y-1 text-lg">
-              <li>Node.js, Express.js</li>
-              <li>MongoDB</li>
+              <li>Financial Analysis</li>
+              <li>Financial Modeling</li>
+              <li>Power BI</li>
+              <li>Excel</li>
+              <li>PowerPoint</li>
             </ul>
           </div>
         </div>
